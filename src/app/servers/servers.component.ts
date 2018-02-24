f0d7948cc94eb69527191d0506976a9c1fcfa922
Andrew <azhadan@homeaway.com>
2018-02-24 15:11:22 +0200

    Initial commit

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,25 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'Testserver';
+  serverCreated = false;
+  servers = ['TestServer#1', 'TestServer#2'];
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.serverCreationStatus = 'Server was created! Name is' + this.serverName;
+  }
+}
